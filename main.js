271cfb86d82e003886462de81dc6178d8228022b
Marcelle Vargas <marcellecode@gmail.com>
2024-06-13 12:12:38 -0300

    [feat]- basic structure and add some style adjustments

---
+++ main.js
@@ -0,0 +1,14 @@
+import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+import backgroundImage from './src/assets/backgroundImage.svg';
+
+document.querySelector('#app').innerHTML = `
+    <div class="hero-container" style="background-image: url(${backgroundImage})">
+        <div class="go-container"></div>
+        <div class="ilustration-container"></div>
+    </div>
+`
+
+// setupCounter(document.querySelector('#counter'))
