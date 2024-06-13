54697c03dc4e8d54882a36454525fe6e91b5af24
Marcelle Vargas <marcellecode@gmail.com>
2024-06-13 19:58:47 -0300

    [style]- update hero style

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-import './style.css'
+import "./style.css";
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
-import backgroundImage from './src/assets/backgroundImage.svg';
-import ilustration from './src/assets/ilustration.svg';
+import backgroundImage from "./src/assets/backgroundImage.svg";
+import ilustration from "./src/assets/ilustration.svg";
+import go from "./src/assets/go.svg";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
     <div class="hero-container" style="background-image: url(${backgroundImage})">
-        <div class="go-container"></div>
-        <div class="ilustration-container">
-            <img class="ilustration-image" src=${ilustration} />
-        </div>
-        <div class="cta-container"></div>
+    <div class="ilustration-container">
+        <img class="ilustration-image" src=${ilustration} />
     </div>
-`
+        <div class="go-container">
+            <p> ラーメン </p>
+            <h1>GO!</h1>
+        </div>
+        <div class="cta-container">
+        <p>
+            Enjoy a good ramen in the comfort of your house.
+            Create your own ramen and 
+            choose your favorite flavour combination!
+        </p>
+        </div>
+    </div>
+`;
 
 // setupCounter(document.querySelector('#counter'))
