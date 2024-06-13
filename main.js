f46712beea0d1cd7a887bd3bcaa3c9bdc2c9d798
Marcelle Vargas <marcellecode@gmail.com>
2024-06-13 13:46:54 -0300

    [style]- style adjustments

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import './style.css'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
 import backgroundImage from './src/assets/backgroundImage.svg';
+import ilustration from './src/assets/ilustration.svg';
 
 document.querySelector('#app').innerHTML = `
     <div class="hero-container" style="background-image: url(${backgroundImage})">
         <div class="go-container"></div>
-        <div class="ilustration-container"></div>
+        <div class="ilustration-container">
+            <img class="ilustration-image" src=${ilustration} />
+        </div>
         <div class="cta-container"></div>
     </div>
 `
