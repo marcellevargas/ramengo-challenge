2a24beab1a7706f795fed66e3c6d9679dd40b830
Marcelle Vargas <marcellecode@gmail.com>
2024-06-17 11:38:04 -0300

    [refactor]- hero section

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 import "./style.css";
+import "./firstSection.css";
 import backgroundImage from "./src/assets/backgroundImage.svg";
 import ilustration from "./src/assets/ilustration.svg";
 import arrow from "./src/assets/arrow.svg";
 import logo from "./src/assets/logo.svg";
 
 document.querySelector("#app").innerHTML = `
-    <header>
-    <img class="ilustration-image" src=${logo} />
-    </header>
-    <div class="hero-container" style="background-image: url(${backgroundImage})">
-
-    <div class="ilustration-container">
-        <img class="ilustration-image" src=${ilustration} />
-    </div>
-        <div class="go-container">
-            <p> ラーメン </p>
-            <h1>GO!</h1>
+    <div class="first-section" style="background-image: url(${backgroundImage})">
+        <div class="sidebar-container">
+            <img class="logo-image" src=${logo} />
         </div>
-        <div class="cta-container">
-        <p>
-            Enjoy a good ramen in the comfort of your house.
-            Create your own ramen and 
-            choose your favorite flavour combination!
-        </p>
-        <button>
-            order now
-            <img class="" src=${arrow} />
-        </button>
+        <div class="hero-container">
+            <div class="ilustration-container">
+                <img class="ilustration-image" src=${ilustration} />
+            </div>
+            <div class="go-container">
+                <div class="go-container">
+                    <p> ラーメン </p>
+                    <h1>GO!</h1>
+                </div>
+            </div>
+            <div class="cta-container">
+                <p>
+                    Enjoy a good ramen in the comfort of your house.
+                    Create your own ramen and 
+                    choose your favorite flavour combination!
+                </p>
+                <button>
+                    order now
+                    <img class="" src=${arrow} />
+                </button>
+            </div>
         </div>
     </div>
 `;
